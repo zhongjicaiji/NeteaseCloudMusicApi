2daf2e404f3be4d31513d5d02992b0dbead69049
zhongjicaiji <1565769040@qq.com>
2023-09-01 17:32:53 +0800

    取消注释

--- util/request.js
+++ util/request.js
@@ -56,7 +56,7 @@ const createRequest = (method, url, data = {}, options) => {
       headers['X-Real-IP'] = ip
       headers['X-Forwarded-For'] = ip
     }
-    // headers['X-Real-IP'] = '118.88.88.88'
+    headers['X-Real-IP'] = '118.88.88.88'
     if (typeof options.cookie === 'object') {
       options.cookie = {
         ...options.cookie,
